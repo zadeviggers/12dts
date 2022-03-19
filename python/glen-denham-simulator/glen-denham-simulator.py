c9f40054ec4dcd04d4dce4bed3024a565145ec8b
Zade Viggers <zade.viggers@gmail.com>
2022-03-19 22:19:10 +1300

    faster rendering by not re-drawing everything
    Signed-off-by: Zade Viggers <zade.viggers@gmail.com>

--- python/glen-denham-simulator/glen-denham-simulator.py
+++ python/glen-denham-simulator/glen-denham-simulator.py
@@ -1,7 +1,8 @@
 import json
+from math import floor
 import os
 import sys
-from time import time
+from time import sleep, time
 import pygame
 
 # Constant
@@ -20,6 +21,8 @@ player_x_velocity = 0
 player_y_velocity = 0
 player_x_position = 0
 player_y_position = 0
+old_player_x_position = 0
+old_player_y_position = 0
 player_jumps_remaining = game["player_allowed_jump_amount"]
 game_is_running = True
 previous_time = time()
@@ -27,10 +30,54 @@ current_level = None
 current_level_number = 0
 game_won = False
 game_lost = False
+lose_win_message_rendered = False
 win_lose_font = None
+# Track parts of the screen that have changed
+dirty_rectangles = []
 
 
 # Functions
+
+
+def draw_player(window: pygame.Surface):
+    global dirty_rectangles
+
+    # Clear out where the player was
+    pygame.draw.rect(window, game["background_colour"],
+                     (old_player_x_position, old_player_y_position, game["player_width"], game["player_height"]))
+    dirty_rectangles.append(
+        (floor(old_player_x_position), floor(old_player_y_position), game["player_width"] + 1, game["player_height"] + 1))
+
+    # Re-draw the player in it's new position
+    pygame.draw.rect(window, game["player_colour"],
+                     (player_x_position, player_y_position, game["player_width"], game["player_height"]))
+    dirty_rectangles.append(
+        (floor(player_x_position), floor(player_y_position), game["player_width"] + 1, game["player_height"] + 1))
+
+
+def draw_level(window: pygame.Surface):
+    global dirty_rectangles
+
+    # Add a black background
+    window.fill(game["background_colour"])
+
+    # Draw objects
+    for object in current_level["layout"]["objects"]:
+        colour = current_level["layout"]["platform_colour"]
+        if object["type"] == "level-end":
+            colour = game["level_end_marker_colour"]
+        pygame.draw.rect(window, colour,
+                         (object["x"], object["y"], object["width"], object["height"]))
+
+    dirty_rectangles.append(
+        (0, 0, window.get_width(), window.get_height()))
+
+
+def draw_everything(window: pygame.Surface):
+    draw_player(window)
+    draw_level(window)
+
+
 def reset_player_position(window: pygame.Surface):
     global game
     global player_x_position
@@ -39,37 +86,47 @@ def reset_player_position(window: pygame.Surface):
     # Put the player in the bottom center
     player_x_position = (window.get_width()//2)-(game["player_width"]//2)
     player_y_position = window.get_height() - game["player_height"]
+    draw_player(window)
 
 
 def load_level(window: pygame.Surface, level_number: int):
     global current_level
     global game_won
+    global dirty_rectangles
 
     if level_number > len(levels)-1:
         game_won = True
 
         # Update window size
-        pygame.display.set_mode((game["width"], game["height"]))
+        pygame.display.set_mode(
+            (game["width"], game["height"]))
 
         # Update window title
         pygame.display.set_caption("You win!" + " - " + game["title"])
     else:
         # Update current level variable
         current_level = levels[level_number]
+        flags = 0
+        if current_level["window"]["resizable"]:
+            flags |= pygame.RESIZABLE
 
         # Update window size
         pygame.display.set_mode(
-            (current_level["window"]["width"], current_level["window"]["height"]))
+            (current_level["window"]["width"], current_level["window"]["height"]), flags=flags)
 
         # Update window title
         pygame.display.set_caption(
             current_level["name"] + " - " + game["title"])
+
+        # Draw level
+        draw_level(window)
 
         # Reset player position
         reset_player_position(window)
 
 
 def on_object_hit(window: pygame.Surface, object) -> bool:
+    # Handle collisions
     global current_level_number
     global current_level
 
@@ -84,6 +141,23 @@ def on_object_hit(window: pygame.Surface, object) -> bool:
         return False
 
 
+def apply_drag(multiplier=1):
+    global player_x_velocity
+    global player_y_velocity
+    if player_x_velocity > 0:
+        player_x_velocity -= game["drag"] * multiplier
+
+        # If the player was slowed down too much, cancel all their horizontal velocity
+        if player_x_velocity < 0:
+            player_x_velocity = 0
+    elif player_x_velocity < 0:
+        player_x_velocity += game["drag"] * multiplier
+
+        # If the player was slowed down too much, cancel all their horizontal velocity
+        if player_x_velocity > 0:
+            player_x_velocity = 0
+
+
 # Start pygame
 pygame.init()
 
@@ -93,16 +167,13 @@ win_lose_font = pygame.font.SysFont(None, 24)
 # Get the window
 window = pygame.display.set_mode((game["width"], game["height"]))
 
+
 # Load the level
 load_level(window, current_level_number)
 
 
-reset_player_position(window)
-
-
 # Game loop
 while game_is_running:
-
     # Loop over events that have happened
     events = pygame.event.get()
     for event in events:
@@ -110,20 +181,31 @@ while game_is_running:
         if event.type == pygame.QUIT:
             game_is_running = False
 
+        # When the user re-sizes the window, re-draw everything
+        if event.type == pygame.VIDEORESIZE:
+            draw_everything(window)
+
     # If game is won or lost
     if game_lost or game_won:
-        message = "You win!"
-        colour = (212, 175, 55)  # Gold
-        if game_lost:
-            message = "You lose"
-            colour = (255, 0, 0)  # Red
+        if not lose_win_message_rendered:
+            lose_win_message_rendered = True
 
-        # Draw black background
-        window.fill(game["background_colour"])
-        # Draw text in center of window
-        text = win_lose_font.render(message, True, colour)
-        window.blit(text, ((window.get_width()//2)-(text.get_width()//2),
-                    (window.get_height()//2)-(text.get_height()//2)))
+            message = "You win!"
+            colour = (212, 175, 55)  # Gold
+            if game_lost:
+                message = "You lose"
+                colour = (255, 0, 0)  # Red
+
+            # Draw black background
+            window.fill(game["background_colour"])
+            # Draw text in center of window
+            text = win_lose_font.render(message, True, colour)
+            window.blit(text, ((window.get_width()//2)-(text.get_width()//2),
+                        (window.get_height()//2)-(text.get_height()//2)))
+
+            # Re-draw screen
+            dirty_rectangles.append(
+                (0, 0, window.get_width(), window.get_height()))
 
     # Gameplay
     else:
@@ -147,7 +229,7 @@ while game_is_running:
                     # If the player has jumps left
                     if player_jumps_remaining > 0:
                         # Player Y position is from the top of the screen down, so subtracting makes it go up
-                        player_y_velocity = game["player_jump_height"] * -1
+                        player_y_velocity = game["player_jump_speed"] * -1
 
                         # Decrement the remaining jumps counter
                         player_jumps_remaining -= 1
@@ -168,10 +250,7 @@ while game_is_running:
         player_y_velocity += game["gravity"] * delta_time
 
         # Apply drag
-        if player_x_velocity > 0:
-            player_x_velocity -= game["drag"] * delta_time
-        elif player_x_velocity < 0:
-            player_x_velocity += game["drag"] * delta_time
+        apply_drag(delta_time)
 
         # Clamp velocity
         if abs(player_x_velocity) > game["player_max_speed"][0]:
@@ -231,6 +310,9 @@ while game_is_running:
                             # Since the player is standing on something, also reset their remaining jumps
                             player_jumps_remaining = game["player_allowed_jump_amount"]
 
+                            # Since the player is standing on something other than the ground, add more drag
+                            apply_drag(game["platform_drag_multiplier"])
+
                 # If the player is moving up...
                 elif player_y_velocity < 0:
 
@@ -279,23 +361,21 @@ while game_is_running:
 
         # Drawing #
 
-        # Clear window
-        window.fill(game["background_colour"])
-
-        # Draw objects
-        for object in current_level["layout"]["objects"]:
-            colour = current_level["layout"]["platform_colour"]
-            if object["type"] == "level-end":
-                colour = game["level_end_marker_colour"]
-            pygame.draw.rect(window, colour,
-                             (object["x"], object["y"], object["width"], object["height"]))
-
-        # Draw the player
-        pygame.draw.rect(window, game["player_colour"],
-                         (player_x_position, player_y_position, game["player_width"], game["player_height"]))
+        # Draw the player if they've moved
+        if player_x_position - old_player_x_position != 0 or player_y_position-old_player_y_position != 0:
+            draw_player(window)
 
     # Render all changes
-    pygame.display.update()
+    if len(dirty_rectangles) > 0:
+        print(dirty_rectangles)
+    pygame.display.update(dirty_rectangles)
+
+    # Empty list of dirty rectangles
+    dirty_rectangles = []
+
+    # Update old player positions
+    old_player_x_position = player_x_position
+    old_player_y_position = player_y_position
 
 
 # Stop game
