9ed0476a18847bdc8dfacd608fbd96b86c196c2a
Zade Viggers <zade.viggers@gmail.com>
2022-07-01 14:21:00 +1200

    homepage adn database improvements
    Signed-off-by: Zade Viggers <zade.viggers@gmail.com>

--- databases/databases-assesment-flask-thingy/app.py
+++ databases/databases-assesment-flask-thingy/app.py
@@ -59,13 +59,13 @@ def cricket_stats():
     if (search_text is not None and search_text.strip() != ""):
         # If a search paramter was provided, search for players
         search = True
-        players = cursor.execute("SELECT Player, Country, Matches, Innings, NotOuts, Runs, HighScore, Ave, BallsFaced, StrikeRate FROM cricket_world_cup_data WHERE Player LIKE ? ORDER BY HighScore DESC",
+        players = cursor.execute("SELECT Player, Country, Matches, Innings, NotOuts, Runs, HighScore, Average, BallsFaced, StrikeRate FROM cricket_world_cup_data WHERE Player LIKE ? ORDER BY HighScore DESC",
                                  ("%" + search_text + "%",)).fetchall()
 
     else:
         # If there wasn't a seach paramater, get list of all players
         players = cursor.execute(
-            "SELECT Player, Country, Matches, Innings, NotOuts, Runs, HighScore, Ave, BallsFaced, StrikeRate FROM cricket_world_cup_data ORDER BY HighScore DESC").fetchall()
+            "SELECT Player, Country, Matches, Innings, NotOuts, Runs, HighScore, Average, BallsFaced, StrikeRate FROM cricket_world_cup_data ORDER BY HighScore DESC").fetchall()
 
     updated_players = []
     for player in players:
@@ -77,7 +77,7 @@ def cricket_stats():
             "not_outs": player["NotOuts"],
             "runs": player["Runs"],
             "high_score": player["HighScore"],
-            "average": player["Ave"],
+            "average": player["Average"],
             "balls_faced": player["BallsFaced"],
             "strike_rate": player["StrikeRate"],
         })
